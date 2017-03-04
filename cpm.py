3355a4912273854c971dd8fbf7476673dedaf8e8
Eric Yu <ffk0716@gmail.com>
2017-03-04 17:25:42 +0800

    bug fix cpm now is importable

--- cpm.py
+++ cpm.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python3
-import argparse
-
-mode = ['simple', 'all']
-
-parser = argparse.ArgumentParser(
-        description='本息平均攤還法 (Constant Payment Mortgage loan, CPM)',
-        epilog="謹慎理財 信用至上")
-parser.add_argument('mode', choices = mode, nargs='?', default = mode[0], help='模式：an integer for the accumulator')
-parser.add_argument('-p', required = True, type=float, help='本金（萬元）(principal)')
-parser.add_argument('-r', required = True, type=float, help='年利率(annual percentage rate)')
-parser.add_argument('-y', required = True, type=int,   help='貸款年數(Years of Loan)')
-args = parser.parse_args()
-
-
-mr = args.r / 100 / 12
-P0 = round(args.p * 10000)
-N = args.y * 12
-
-R = (1 + mr) ** N
-c = (R * mr) / (R - 1) * P0
-
-c = round(c)
+    
+def count(**args):
+    mr = args['rate'] / 100 / 12
+    N = args['year'] * 12
+    R = (1 + mr) ** N
+    class temp: pass
+    r = temp()
+    r.cr = (R * mr) / (R - 1)
+    r.c = round(r.cr * args['principal'])
+    r.total = r.c * N
+    r.total_int = r.total - args['principal']
+    return r
 
 def print2d(t):
     assert len(t) > 0
@@ -34,27 +24,44 @@ def print2d(t):
         table.append('  '.join(row2))
     print('\n'.join(table))
 
-def p(v):
-    return '{:.1f}%'.format(v * 100)
-
-table = []
-table.append(['本金：', '{}萬元'.format(args.p)])
-table.append(['貸款年數：', '{}年'.format(args.y)])
-table.append(['年利率：', '{}%'.format(args.r)])
-table.append(['每月攤還金額：', '{}元'.format(c)])
-total_interest = c * N - P0
-table.append(['全部利息：', '{}元'.format(total_interest)])
-table.append(['利息本金比：', p(total_interest / P0)])
-
-print2d(table)
-
-
-rP = P0
-if args.mode == 'all':
-    table = [['期數', '攤還本金', '攤還利息', '剩餘本金', '剩餘本金百分比']]
-    for i in range(N):
-        t = rP * mr 
-        t = round(t)
-        rP -= (c - t)
-        table.append([i + 1, c - t, t, rP, p(rP / P0)])
+if __name__ == '__main__':
+    import argparse
+    
+    mode = ['simple', 'all']
+    
+    parser = argparse.ArgumentParser(
+            description='本息平均攤還法 (Constant Payment Mortgage loan, CPM)',
+            epilog="謹慎理財 信用至上")
+    parser.add_argument('mode', choices = mode, nargs='?', default = mode[0], help='模式：an integer for the accumulator')
+    parser.add_argument('-p', required = True, type=float, help='本金（萬元）(principal)')
+    parser.add_argument('-r', required = True, type=float, help='年利率(annual percentage rate)')
+    parser.add_argument('-y', required = True, type=int,   help='貸款年數(Years of Loan)')
+    args = parser.parse_args()
+    
+    mr = args.r / 100 / 12
+    P0 = round(args.p * 10000)
+    N = args.y * 12
+    c = count(year = args.y, rate = args.r, principal = P0).c
+    
+    def p(v):
+        return '{:.1f}%'.format(v * 100)
+    
+    table = []
+    table.append(['本金：', '{}萬元'.format(args.p)])
+    table.append(['貸款年數：', '{}年'.format(args.y)])
+    table.append(['年利率：', '{}%'.format(args.r)])
+    table.append(['每月攤還金額：', '{}元'.format(c)])
+    total_interest = c * N - P0
+    table.append(['全部利息：', '{}元'.format(total_interest)])
+    table.append(['利息本金比：', p(total_interest / P0)])
     print2d(table)
+    
+    rP = P0
+    if args.mode == 'all':
+        table = [['期數', '攤還本金', '攤還利息', '剩餘本金', '剩餘本金百分比']]
+        for i in range(N):
+            t = rP * mr 
+            t = round(t)
+            rP -= (c - t)
+            table.append([i + 1, c - t, t, rP, p(rP / P0)])
+        print2d(table)
--- rcpm.py
+++ rcpm.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 import argparse
-
-mode = ['simple', 'all']
+import cpm
 
 parser = argparse.ArgumentParser(
         description='本息平均攤還法 (Constant Payment Mortgage loan, CPM)',
         epilog="謹慎理財 信用至上")
-parser.add_argument('mode', choices = mode, nargs='?', default = mode[0], help='模式：an integer for the accumulator')
 parser.add_argument('-p', required = True, type=float, help='本金（萬元）(principal)')
 parser.add_argument('-i', required = True, type=float, help='總利息（萬元）(toal interest)')
 parser.add_argument('-y', required = True, type=int,   help='貸款年數(Years of Loan)')
@@ -15,10 +13,8 @@ args = parser.parse_args()
 P0 = round(args.p * 10000)
 N = args.y * 12
 
-def get_ti(mr):
-    R = (1 + mr) ** N
-    c = (R * mr) / (R - 1) * P0
-    return c * N - P0
+def get_ti(r):
+    return cpm.count(year = args.y, rate = r, principal = P0).total_int
 
 def frange(x, y, jump):
   while x < y:
@@ -27,8 +23,7 @@ def frange(x, y, jump):
 
 ti = args.i * 10000
 for r in frange(0.001, 100, 0.0001):
-    mr = r / 100 / 12
-    ti2 = get_ti(mr)
+    ti2 = get_ti(r)
     if ti2 > ti:
         print('年利率約為{:.3f}%'.format(r))
         break 
